0e8009f0701625e2b2a67e348f23222421a5d336
8bitretro <christian.langlois04@gmail.com>
2020-08-16 13:17:06 -0500

    added code

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+
+namespace Character_Counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string appname = "Character Counter";
+            string author = "8bitretro";
+
+            Console.WriteLine(appname + " by " + author);
+            Console.WriteLine();
+
+            Console.WriteLine("Enter a string of text...");
+            string text = Console.ReadLine();
+            Console.WriteLine();
+
+            int textLength = text.Length;
+            Console.WriteLine("The number of characters in the text is : {0}", textLength);
+
+
+            Console.ReadLine();
+            
+
+
+            
+            
+
+
+
+
+
+
+        }
+    }
+}
